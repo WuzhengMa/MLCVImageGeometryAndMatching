4492bb61b4052139a11c8873fa29256ffd826cec
Wuzheng Ma <oliverm0919@gmail.com>
2017-04-20 19:09:12 +0100

    parameter changes added

--- cw_main.m
+++ cw_main.m
@@ -2,8 +2,8 @@ init;
 patchSize = 32;
 
 %% Q1.2.a) Harris interest point detector
-imageName1 = 'FDFigures/DSC02736.ppm';
-imageName2 = 'FDFigures/DSC02737.ppm';
+imageName1 = 'FDFigures/plant2.ppm';
+imageName2 = 'FDFigures/plant3.ppm';
 
 if size(size(imread(imageName1)),2) == 2
     imgExample1 = (imread(imageName1));
@@ -14,8 +14,8 @@ else
 end
 
 
-[x1, y1] = harrisDetector(imageName1, patchSize, 1000, 10);
-[x2, y2] = harrisDetector(imageName2, patchSize, 1000, 10);
+[x1, y1] = harrisDetector(imageName1, patchSize, 100, 10);
+[x2, y2] = harrisDetector(imageName2, patchSize, 100, 10);
 
 
 %% Q1.2.b)
@@ -45,6 +45,8 @@ hold on;
 plot(y2,x2,'rx');
 hold off;
 
+
+
 %[descriptors1, descriptors2, x1, y1, x2, y2] = matchDescriptorSize(descriptors1', descriptors2', features1(1,:)', features1(2,:)', features2(1,:)', features2(2,:)', 'Norm8Points');
 %[descriptors1, descriptors2, x1, y1, x2, y2] = matchDescriptorSize(descriptors1', descriptors2', features1(1,:)', features1(2,:)', features2(1,:)', features2(2,:)', 'RANSAC');
 %[descriptors1, descriptors2, x1, y1, x2, y2] = matchDescriptorSize(descriptors1, descriptors2, x1, y1, x2, y2, 'Norm8Points');
@@ -66,27 +68,27 @@ hold on;
 plot(y2,x2,'rx');
 hold off;
 
-
 %% Q1.2.c)
 %nearestIndex[i] = x means that ith descriptor1 is nearest to xth 
 %descriptor2
 %[nearestIndex, distance] = knnsearch(descriptors2, descriptors1, 'Distance', 'cityblock');
 
 %% Q1.3.a) Q1.3.c)
-[h, status1] = getHomographyMatrix(x1, y1, x2, y2, 'RANSAC', 2000);
+[h, status1] = getHomographyMatrix(x1, y1, x2, y2, 'RANSAC', 10000);
 
 %Obtain the projection points from image 2 to image 1
-homoTransPoints = h\[x2;y2;ones(1,size(x2,2))];
+homoTransPoints = h*[x1;y1;ones(1,size(x1,2))];
 oneOverHomoZ=(1./homoTransPoints(3,:));
 oneOverHomoZ=[oneOverHomoZ; oneOverHomoZ; oneOverHomoZ];
 transPoints = oneOverHomoZ.*homoTransPoints; %Change from homogeneous coordinate to imhomogeneous coordinates
 
 %Calculate the homography accuracy HA
-[HA, HD] = getHomoAccuracy([x1; y1], transPoints([1,2], :));
+[HA, HD] = getHomoAccuracy([x2; y2], transPoints([1,2], :));
 
 % Draw the projected point back to image 1
-imshow(imageName1);
-title('Projected figure from image 2 to image 1');
+figure;
+imshow(imageName2);
+title('Projected figure from image 1 to image 2');
 hold on;
 plot(transPoints(2,:), transPoints(1,:), 'rx');
 hold off;
@@ -99,7 +101,7 @@ hold off;
 
 %Using own implementation
 %[F,inliersIndex] = getFundamentalMatrix([x1', y1'],[x2', y2'], 'Norm8Points', 1);
-[F,inliersIndex] = getFundamentalMatrix([x1', y1'],[x2', y2'], 'RANSAC', 2000);
+[F,inliersIndex] = getFundamentalMatrix([x1', y1'],[x2', y2'], 'RANSAC', 10000);
 
 %Get epipoles
 [e1, e2] = getEpipoles(F);
@@ -128,12 +130,17 @@ line(points(:,[2,4])',points(:,[1,3])');
 hold off;
 
 
-%% Q2.2.C) Compute the disparity map                             
+%% Q2.2.C) Compute the disparity map    
+[t1, t2] = estimateUncalibratedRectification(F,[x1', y1'], [x2', y2'], size(imgExample1));
+[I1Rect,I2Rect] = rectifyStereoImages(imgExample1,imgExample2,t1,t2);
+figure;
+imshow(stereoAnaglyph(I1Rect,I2Rect));
+
 figure;
 subplot(1,2,1);
 title('Red-cyan graph of two images');
 imshow(stereoAnaglyph(imgExample1,imgExample2));
-imtool(stereoAnaglyph(imgExample1,imgExample2));
+%imtool(stereoAnaglyph(imgExample1,imgExample2));
 disparityRange = [0, 96];
 disparityMap = disparity(imgExample1, imgExample2, 'BlockSize', 15, 'DisparityRange', disparityRange);
 subplot(1,2,2);
